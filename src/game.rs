a70497c0951d71e10e934f20792aea4f0b43c7d5
senpai-10 <bmjfdrh@gmail.com>
2022-05-14 06:46:36 +0300

    game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,22 @@
+// TODO rewrite code
+
 use crate::skip_fail;
+use std::io::Read;
 use std::io::Write;
+
 pub struct Game {
     board: Vec<Vec<String>>,
     turn: String,
+    players_moves: Players,
     quit: bool,
 }
 
+struct Players {
+    x: Vec<u8>,
+    o: Vec<u8>,
+}
+// TODO handle draw
+// TODO add colors to x and o chars
 impl Game {
     pub fn new() -> Game {
         Game {
@@ -15,6 +26,10 @@ impl Game {
                 vec![7.to_string(), 8.to_string(), 9.to_string()],
             ],
             turn: String::from("x"),
+            players_moves: Players {
+                x: vec![0, 0, 0, 0, 0, 0, 0, 0],
+                o: vec![0, 0, 0, 0, 0, 0, 0, 0],
+            },
             quit: false,
         }
     }
@@ -22,7 +37,18 @@ impl Game {
     pub fn run(mut self) {
         while !self.quit {
             self.clear_screen();
-            println!();
+            let winner = self.find_winner();
+
+            if winner.is_empty() == false {
+                println!("{} has won!!!!", winner);
+                self.pause();
+                self.reset();
+            }
+
+            self.clear_screen();
+
+            // println!("x moves: {:?}", self.players_moves.x);
+            // println!("o moves: {:?}", self.players_moves.o);
             self.display();
             let input: u8 = skip_fail!(self.input(&format!("{}'s Turn ", self.turn)).parse());
             self.fill_cell(input, self.turn.to_owned());
@@ -39,24 +65,55 @@ impl Game {
         }
     }
 
-    fn is_board_full(&self) {
-        todo!()
-    }
-
     fn fill_cell(&mut self, cell_number: u8, turn: String) {
-        // self.find_winner();
+        let player = match turn.as_str() {
+            "x" => &mut self.players_moves.x,
+            "o" => &mut self.players_moves.o,
+            _ => &mut self.players_moves.x,
+        };
 
         for (row_index, row) in self.board.clone().iter().enumerate() {
-            for (item_index, item) in row.iter().enumerate() {
+            for (col_index, item) in row.iter().enumerate() {
                 if item == &cell_number.to_string() {
-                    self.board[row_index][item_index] = turn.to_string();
+                    self.board[row_index][col_index] = turn.to_string();
+                    player[row_index] += 1;
+                    player[col_index + 3] += 1;
+                    if row_index == col_index {
+                        player[6] += 1;
+                    }
+                    if row_index == 2 - col_index {
+                        player[7] += 1;
+                    }
                 }
             }
         }
     }
 
-    fn find_winner(&self) {
-        todo!()
+    fn find_winner(&self) -> String {
+        let mut winner = String::new();
+
+        for i in 0..8 {
+            if self.players_moves.x[i] == 3 {
+                winner = "x".to_string();
+            } else if self.players_moves.o[i] == 3 {
+                winner = "o".to_string();
+            }
+        }
+
+        return winner;
+    }
+
+    fn reset(&mut self) {
+        self.board = vec![
+            vec![1.to_string(), 2.to_string(), 3.to_string()],
+            vec![4.to_string(), 5.to_string(), 6.to_string()],
+            vec![7.to_string(), 8.to_string(), 9.to_string()],
+        ];
+        self.turn = String::from("x");
+        self.players_moves = Players {
+            x: vec![0, 0, 0, 0, 0, 0, 0, 0],
+            o: vec![0, 0, 0, 0, 0, 0, 0, 0],
+        };
     }
 
     fn next_turn(&mut self) {
@@ -80,5 +137,18 @@ impl Game {
 
     fn clear_screen(&self) {
         print!("\x1B[2J\x1B[1;1H");
+    }
+
+    // source: https://users.rust-lang.org/t/rusts-equivalent-of-cs-system-pause/4494/4
+    fn pause(&self) {
+        let mut stdin = std::io::stdin();
+        let mut stdout = std::io::stdout();
+
+        // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+        write!(stdout, "Press any key to continue...").unwrap();
+        stdout.flush().unwrap();
+
+        // Read a single byte and discard
+        let _ = stdin.read(&mut [0u8]).unwrap();
     }
 }
